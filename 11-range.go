880f21937793c289e01d4f0079d0ee6086af107c
Chad McElligott <chad.mcelligott@gmail.com>
2022-06-05 13:34:35 -0500

    finished range

--- 11-range.go
+++ 11-range.go
@@ -9,4 +9,23 @@ func main() {
 		sum += num
 	}
 	fmt.Println("sum", sum)
+
+	for i, num := range nums {
+		if num == 3 {
+			fmt.Println("index:", i)
+		}
+	}
+
+	kvs := map[string]string{"a": "apple", "b": "banana"}
+	for k, v := range kvs {
+		fmt.Printf("%s -> %s\n", k, v)
+	}
+
+	for k := range kvs {
+		fmt.Println("key:", k)
+	}
+
+	for i, c := range "gołang" { // i = starting index of rune
+		fmt.Println(i, c)
+	}
 }
